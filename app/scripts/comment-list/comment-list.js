165f5a97ce952d34fc9429dab89f092b792249c8
Sean Hickey <shickey@blakeschool.org>
2015-10-08 09:23:03 -0500

    Scrolling to comments works roughly correctly

--- app/scripts/comment-list/comment-list.js
+++ app/scripts/comment-list/comment-list.js
@@ -3,9 +3,6 @@
 angular.module('codeReviewApp')
   .directive('commentList', function() {
     return {
-      link: function(scope, element, attrs) {
-        scope.listElement = element;
-      },
       templateUrl: 'scripts/comment-list/comment-list.html',
       controller: 'CommentListCtrl',
       controllerAs: 'ctrl'
@@ -16,9 +13,10 @@ angular.module('codeReviewApp')
     $scope.$watch('selectedComments', function(selectedComments) {
       if (selectedComments.length === 0) { return; }
       var commentId = selectedComments[0].id;
-      var el = angular.element(document.querySelector('#comment-' + commentId));
-      var top = el.position().top;
-      $scope.listElement.css({top: -top, position: 'absolute'});
+      var commentEl = angular.element(document.querySelector('#comment-' + commentId));
+      var top = commentEl.position().top;
+      var listEl = angular.element(document.querySelector('#comment-list'));
+      listEl.css({top: -top});
     });
     
   }]);
