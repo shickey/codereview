3c8bf89b1fe4062a6dec2b0214bdb1f2fa719076
Sean Hickey <shickey@blakeschool.org>
2015-10-29 23:36:25 -0500

    Switched order of ace includes

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -23,9 +23,9 @@ module.exports = function(config) {
     files: [
       // bower:js
       'bower_components/jquery/dist/jquery.js',
-      'bower_components/ace-builds/src-min/ace.js',
       'bower_components/ace-builds/src-min/theme-monokai.js',
       'bower_components/ace-builds/src-min/mode-python.js',
+      'bower_components/ace-builds/src-min/ace.js',
       'bower_components/angular/angular.js',
       'bower_components/angular-animate/angular-animate.js',
       'bower_components/angular-aria/angular-aria.js',
