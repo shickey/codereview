f1a43148f48d37dc9a81ea5f2966d61519825c31
Sean Hickey <shickey@blakeschool.org>
2015-11-09 07:03:36 -0600

    Added filter for accetpable mime types

--- app/scripts/folder.js
+++ app/scripts/folder.js
@@ -11,11 +11,22 @@
 angular.module('codeReviewApp')
   .controller('FolderCtrl', ['$scope', '$stateParams', 'drive', function($scope, $stateParams, drive) {
     
+    $scope.ACCEPTABLE_MIME_TYPES = ['application/vnd.google-apps.folder', 'text/x-python'];
+    
     $scope.files = [];
     $scope.folderId = $stateParams.folderId;
+    $scope.fileId = undefined;
     
     drive.fetchChildrenOfFolder($stateParams.folderId).then(function(children) {
       $scope.files = children;
-    })
+    });
+    
+    $scope.$on('$stateChangeSuccess', function(e, toState, toParams, fromState, fromParams) {
+      $scope.fileId = toParams.fileId;
+    });
+    
+    $scope.mimeTypeFilter = function(folderChild) {
+      return $scope.ACCEPTABLE_MIME_TYPES.indexOf(folderChild.mimeType) !== -1;
+    };
     
   }]);
